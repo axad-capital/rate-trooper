acb1749aebc3a857e213116255a6a7e5aac4f43c
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-05 14:52:56 -0400

    fixed ads

--- client/src/components/NavComp/NavComp.js
+++ client/src/components/NavComp/NavComp.js
@@ -5,33 +5,50 @@ import Trooper from './RateTrooperLogo.png'
 
 const NavComp = () => {
 
+    function home() {
+        window.location.href = '/'
+    }
+
     const location = useLocation()
 
-  return (
-    <div>
-         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <Link to='/' className='navbar-brand'><img className='trooper-logo' src={Trooper} alt="trooper" /></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link to='/' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <a href='#how-it-works-container' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>How It Works</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href='#about-us' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>About Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-    </div>
-  )
+    if (location.pathname === '/thanks') {
+        return (
+            <div>
+               <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                      <div className="container-fluid">
+                          <img onClick={home} style={{cursor: 'pointer'}} className='navbar-brand trooper-logo' src={Trooper} alt="trooper" />
+                      </div>
+                  </nav>
+          </div>
+        )
+    } else {
+        return (
+          <div>
+               <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                      <div className="container-fluid">
+                          <img onClick={home} style={{cursor: 'pointer'}} className='navbar-brand trooper-logo' src={Trooper} alt="trooper" />
+                          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                              <span className="navbar-toggler-icon"></span>
+                          </button>
+                          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                              <ul className="navbar-nav">
+                                  <li className="nav-item">
+                                      <Link to='/' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
+                                  </li>
+                                  <li className="nav-item">
+                                      <a href='#how-it-works-container' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>How It Works</a>
+                                  </li>
+                                  <li className="nav-item">
+                                      <a href='#about-us' className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>About Us</a>
+                                  </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </nav>
+          </div>
+        )
+    }
+
 }
 
 export default NavComp
