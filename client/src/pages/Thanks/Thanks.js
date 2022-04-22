a023c730e4a9a1442ad82bfb83b973daa5915634
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-22 11:19:52 -0400

    added old pixel

--- client/src/pages/Thanks/Thanks.js
+++ client/src/pages/Thanks/Thanks.js
@@ -6,6 +6,7 @@ const Thanks = () => {
         <div>
             <h1>Compare Insurance Quotes in Your Area. Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.</h1>
             {/* Offer Conversion: RateTrooper - Affordable Auto Insurance Quotes - US */}
+            <iframe title='pixel' src="https://offers.axtracking.com/aff_l?offer_id=1270" scrolling="no" frameborder="0" width="1" height="1"></iframe>
             <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1270&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
             {/* End Offer Conversion */}
         </div>
